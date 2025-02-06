aac5957a0e3e76317079f8dfaf63f46c74bbdfc1
dongchangqing <cdon822@aucklanduni.ac.nz>
2025-02-07 09:47:19 +1300

    solve master-devel conflict

--- user_run_files/opencor_pythonshell_path.sh
+++ user_run_files/opencor_pythonshell_path.sh
@@ -1,2 +1,18 @@
+<<<<<<< Updated upstream
 opencor_pythonshell_path=/home/cdon822/software/OpenCOR-0-7-1-Linux/pythonshell
 
+=======
+#_______python paths _________
+# Users should modify opencor_pythonshell_path to the path of their own opencor pythonshell
+# the below for my hpc
+# opencor_pythonshell_path=/hpc/farg967/OpenCOR-2022-05-23-Linux/pythonshell
+# opencor_pythonshell_path=/hpc/farg967/OpenCOR-0-7-1-Linux/pythonshell
+# the below for my local
+# opencor_pythonshell_path=/home/farg967/software/OpenCOR-0-7-1-Linux/pythonshell
+#<<<<<<< Updated upstream
+#opencor_pythonshell_path=/home/farg967/software/OpenCOR-2024-11-29-Linux/pythonshell
+#=======
+#opencor_pythonshell_path=/home/farg967/software/OpenCOR-2024-08-23-Linux/pythonshell
+#opencor_pythonshell_path=/home/cdon822/software/OpenCOR-0-7-1-Linux/pythonshell
+#>>>>>>> Stashed changes
+opencor_pythonshell_path=/home/cdon822/software/OpenCOR-0-7-1-Linux/pythonshell
